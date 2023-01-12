cd9c5fdbbebfb325f895005e2d6886c66a18440b
sushil <sushil.sawai@wissen.com>
2023-01-12 11:49:47 +0530

    Hashmap code added

--- JavaCollection/src/BasicJava/Test.java
+++ JavaCollection/src/BasicJava/Test.java
@@ -1,10 +1,24 @@
 package BasicJava;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HashMap<String, Integer> map = new HashMap<>();
+		 
+        // Add elements using put method
+        map.put("vishal", 10);
+        map.put("sachin", 30);
+        map.put("vaibhav", 20);
+        
+        for(Map.Entry<String, Integer> e: map.entrySet())
+        {
+        	System.out.println(e.getKey()+" "+e.getValue());
+        }
+ 
 	}
 
 }
